5b4bc9285bcb8aebb5270ed0013fe930ee02d9ea
Parth Sharma <2023kucp1020@iiitkota.ac.in>
2024-11-16 23:34:07 +0530

    final commit

---
+++ BACKENED/src/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './DB/connection.js';
+import router from './router/staticRoutes.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+dotenv.config({
+    path: './env'
+});
+
+const app = express();
+
+// Connect to the database
+connectDB();
+
+app.use(cors({
+    origin: "https://1-mern-project-frontend.vercel.app",
+    credentials: true
+}))
+
+app.use(cookieParser());  // Allows you to access and set cookies, enabling CRUD operations on cookies
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', router);
+
+const PORT = process.env.PORT || 3000;  
+
+
+
+app.listen(PORT, () => {
+   console.log(`server is listneing at ${PORT}`)
+});
+
+
